0f149d6bdf309720575ab01a752df8232102cbbe
maximus9802 <vuquy090802@gmail.com>
2024-10-28 22:46:51 +0700

    add set up for esp32cam

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,65 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include "esp_camera.h"
+#include <WiFi.h>
+
+#define CAMERA_MODEL_AI_THINKER 
+
+#include "camera_pins.h"
+
+// define constant
+#define ESP32CAM_LED_FLASH 4
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  Serial.setDebugOutput(true);
+  Serial.println();
+  Serial.println("MYCO CAMERA V1");
+  Serial.println();
+
+  // FLASH LED
+  pinMode(ESP32CAM_LED_FLASH, OUTPUT);
+  digitalWrite(ESP32CAM_LED_FLASH, LOW);
+
+  // buffer.reserve(32000);
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000; // was at 20
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA; //800 x 600 necessary for Adafruit IO
+  config.jpeg_quality = 30;
+  config.fb_count = 1;
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  else {
+    Serial.printf("Camera init success");
+  }
 }
 
 void loop() {
