2b2e141b56c699ee8d552006bbe949860eeb9991
maximus9802 <vuquy090802@gmail.com>
2024-12-09 00:48:37 +0700

    update logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include <ESP32Servo.h>
+#include <base64.h>
 
 #define CAMERA_MODEL_AI_THINKER 
 
@@ -13,24 +14,32 @@
 
 // Define constant
 #define ESP32CAM_LED_FLASH 4
-#define AWS_IOT_PUBLISH_TOPIC   ""      // Change data
-#define AWS_IOT_SUBSCRIBE_TOPIC ""      // Change data
+#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"      // Change data
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"      // Change data
 #define SOUND_SPEED 0.034
-#define CM_TO_INCH 0.393701
-#define LIMIT_DISTANCE 80
+// #define CM_TO_INCH 0.393701
+#define LIMIT_DISTANCE 50
 
 const int trigPin = 14;
 const int echoPin = 15;
+const int servoPin = 13;
+bool inDock = false;
+bool isProcessing = false;
+int countWaiting = 0;
 
 // Declear methods
-int myFunction(int, int);
 void initCamera();
 void connectWiFi();
 void connectAWS();
 void controlServo(boolean);
-boolean detectedObject();
+boolean detectedObjectToCapture(boolean);
 void messageHandler(char* topic, byte* payload, unsigned int length);
 boolean publishMessage(String buffer);
+void initServo();
+void controlSystem();
+boolean captureImageAndSend();
+void autoCloseBarie(int);
+
 
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
@@ -39,6 +48,8 @@ Servo servo;
 void setup() {
   // Initialize camera
   initCamera();
+
+  initServo();
 
   // Connect WiFi
   connectWiFi();
@@ -51,9 +62,91 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("------------------Start Loop------------------");
+  controlSystem();
   client.loop();
-  Serial.println("------------------Loop------------");
+  autoCloseBarie(countWaiting);
+  Serial.println("------------------End   Loop------------------");
+  countWaiting = countWaiting + 1;
   delay(1000);
+}
+
+void autoCloseBarie(int time) {
+  if (time > 10) {
+    controlServo(false);
+    countWaiting = 0;
+  }
+}
+
+boolean captureImageAndSend() {
+  Serial.println("captureImageAndSend: start");
+  bool status = false;
+  digitalWrite(ESP32CAM_LED_FLASH, HIGH);
+  camera_fb_t *fb = NULL;
+  fb = esp_camera_fb_get();
+
+  if (!fb){
+    Serial.println("Camera capture failed");
+    delay(2000);
+    Serial.println("captureImageAndSend: end");
+    return status;
+  }
+  else {
+    Serial.println("Camera Captured");
+  }
+
+  digitalWrite(ESP32CAM_LED_FLASH, LOW);
+
+  String buffer = base64::encode((uint8_t *)fb->buf, fb->len);
+
+  unsigned int length();
+  Serial.println("Buffer Length: ");
+  Serial.print(buffer.length());
+  Serial.println("");
+
+  if (buffer.length() > 30000)
+  {
+    Serial.println("Image size too big");
+    buffer = "";
+    delay(2000);
+    return status;
+  }
+
+  Serial.print("Publishing...");
+  Serial.print("Publishing...");
+  Serial.println("");
+  if (publishMessage(buffer)) {
+    status = true;
+    Serial.print("Published");
+  }
+  else {
+    Serial.println("Error");
+  }
+
+  buffer = "";
+  esp_camera_fb_return(fb);
+
+  Serial.println("captureImageAndSend: end");
+  return status;
+}
+
+void controlSystem() {
+  if (inDock == true) {
+    Serial.println("Dock has been rented!");
+    return;
+  }
+
+  if (isProcessing == true) {
+    Serial.println("In processing...");
+    return;
+  }
+
+  if (detectedObjectToCapture(true)) {
+    if (captureImageAndSend()) {
+      isProcessing = true;
+    }
+    return;
+  }
 }
 
 void connectWiFi() {
@@ -69,6 +162,13 @@ void connectWiFi() {
   }
 
   Serial.println("Connectted to Wi-Fi");
+}
+
+void initServo() {
+  servo.attach(servoPin, 500, 2400);
+
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 }
 
 void messageHandler(char* topic, byte* payload, unsigned int length) {
@@ -95,6 +195,7 @@ void messageHandler(char* topic, byte* payload, unsigned int length) {
     Serial.println(controlBarie);
     if (strcmp(controlBarie, "open") == 0) {
       controlServo(true);
+      inDock = true;
     } else if (strcmp(controlBarie, "close") == 0) {
       controlServo(false);
     } else {
@@ -104,22 +205,24 @@ void messageHandler(char* topic, byte* payload, unsigned int length) {
   } else {
     Serial.println("Key 'control' not found in JSON.");
   }
+  isProcessing = false;
 }
 
 boolean publishMessage(String buffer) {
-  StaticJsonDocument<80000> doc;
+  StaticJsonDocument<30100> doc;
+  doc["slotId"] = SLOT_ID;
   doc["image"] = buffer;
-  doc["dockId"] = DOCK_ID;
 
   String json_payload;
   serializeJson(doc, json_payload);
 
   if (client.publish(AWS_IOT_PUBLISH_TOPIC, json_payload.c_str())) {
     Serial.println("JSON image sent");
+    return true;
   } else {
     Serial.println("Failed to send JSON image");
+    return false;
   }
-
 }
 
 void connectAWS() {
@@ -188,9 +291,9 @@ void initCamera() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000; // was at 20
   config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_SVGA; //800 x 600 necessary for Adafruit IO
-  config.jpeg_quality = 30;
-  config.fb_count = 1;
+  config.frame_size = FRAMESIZE_QVGA; //800 x 600 necessary for Adafruit IO
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
@@ -200,7 +303,7 @@ void initCamera() {
     return;
   }
   else {
-    Serial.printf("Camera init success");
+    Serial.println("Camera init success");
   }
 }
 
@@ -209,11 +312,13 @@ void controlServo(boolean isOpen) {
     servo.write(0);
     Serial.println("Open door!");
   } else {
+    Serial.println("Close door!");
     servo.write(90);
   }
 }
 
-boolean detectedObject() {
+boolean detectedObjectToCapture(boolean test) {
+  Serial.println("detectedObjectToCapture: start");
   long duration;
   float distanceCm;
 
@@ -225,18 +330,17 @@ boolean detectedObject() {
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
+  Serial.println(duration);
   
   // Calculate the distance
   distanceCm = duration * SOUND_SPEED/2;
+  Serial.println(distanceCm);
 
   if (distanceCm < LIMIT_DISTANCE) {
     Serial.println("Detected object!!!");
+    Serial.println("detectedObjectToCapture: end");
     return true;
-  } 
+  }
+  Serial.println("detectedObjectToCapture: end"); 
   return false;
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
--- src/secrets.h
+++ src/secrets.h
@@ -1,12 +1,12 @@
 #include <pgmspace.h>
 
 #define SECRET
-#define THINGNAME ""                    // Change data
+#define THINGNAME "ESP32CAM"                    // Change data
 
 const char WIFI_SSID[] = "";            // Change data
 const char WIFI_PASSWORD[] = "";        // Change data
-const char AWS_IOT_ENDPOINT[] = "";     // Change data
-const char DOCK_ID[] = "DOCK00001";     // Change data
+const char AWS_IOT_ENDPOINT[] = "a1e4360w212q2k-ats.iot.ap-southeast-1.amazonaws.com";     // Change data
+const int SLOT_ID = 2;    // Change data
 
 // Amazon Root CA 1
 static const char AWS_CERT_CA[] PROGMEM = R"EOF(
